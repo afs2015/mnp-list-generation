34d29c4743ff22224ae52815d61bbe2e78b4b9a8
Andrew Selzer <andrew.selzer@americanwell.com>
2019-02-27 14:34:45 -0500

    Initial commit

---
+++ mnp-parse.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('input/SampleMNPFile.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+
+    for row in csv_reader:
+        print(row)
