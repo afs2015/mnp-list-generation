f8aca787011c84488b7a2c397dac8c17046f0bd7
Andrew Selzer <andrew.selzer@americanwell.com>
2019-02-27 14:47:39 -0500

    Add ability to specify a file

--- mnp-parse.py
+++ mnp-parse.py
@@ -1,8 +1,16 @@
 import csv
+import sys
 
-with open('input/SampleMNPFile.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
+def main ():
+    # Check if filename provided
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+    else:
+        file_name = 'input/SampleFile.csv'
 
-    for row in csv_reader:
-        print(row)
+    with open(file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+
+        for row in csv_reader:
+            print(row)
