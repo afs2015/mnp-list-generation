8382f8979ec0af69c1e64757a113f95fe952729e
Andrew Selzer <andrew.selzer@americanwell.com>
2019-02-28 12:13:13 -0500

    Moved opening file to seperate function

--- mnp-parse.py
+++ mnp-parse.py
@@ -3,12 +3,33 @@ import itertools
 import sys
 from operator import itemgetter
 
+def create_unparsed_list(unparsed_csv):
+    """ This takes in an unparsed_csv (str) and returns a list of
+        unparsed strings representing entries to a dance. """
+
+    # Open unparsed csv
+    with open(unparsed_csv) as infile:
+        csv_reader = csv.reader(infile, delimiter=',')
+
+        # Skip first three lines
+        for skip in range(3):
+            next(csv_reader)
+
+        entry_list = []
+        for row in csv_reader:
+            entry_list.append([row[0].lower(), row[1].lower()])
+
+    return entry_list
+
 def main ():
+
     # Check if filename provided
     if len(sys.argv) > 1:
-        unparsed_csv = sys.argv[1]
+        unparsed_csv  = sys.argv[1]
     else:
-        unparsed_csv = 'input/SampleFile.csv'
+        unparsed_csv  = 'input/SampleFile.csv'
+
+    raw_entry_list = create_unparsed_list(unparsed_csv)
 
     # Open unparsed csv
     with open(unparsed_csv) as infile:
@@ -26,10 +47,10 @@ def main ():
         entry_list.sort(key=itemgetter(0))
 
         # Remove Duplicates
-        # entry_list = list(entry_list for entry_list,_ in itertools.groupby(entry_list))
+        entry_list = list(entry_list for entry_list,_ in itertools.groupby(entry_list))
 
     # Open outfile to write to
-    with open('output/output_file.csv', mode='w') as outfile:
+    with open('output_file.csv', mode='w') as outfile:
         entry_writer = csv.writer(outfile, delimiter=',',quotechar='"')
 
         for entry in entry_list:
